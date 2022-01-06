a98587919c3eca483818facf43cbc3b3a59e8354
Mahesh Gaikwad <gaikwadmahesh777g@gmail.com>
2022-01-06 18:24:12 +0530

    Day5

--- Day_5_Shuffle String/solution.cpp
+++ Day_5_Shuffle String/solution.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-string restoreString(string s, vector<int>& indices) {
-    string str = s;
-    for(int i=0;i<indices.size();i++){
-       str[indices[i]] = s[i];
+  string restoreString(string s, vector<int>& indices) {
+      string str = s;
+      for(int i=0;i<indices.size();i++){
+        str[indices[i]] = s[i];
+      }
+      return str;
     }
-    return str;
-  }
 };
